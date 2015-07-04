d36bac2402576ebb4ebbbda90dc9b773c4bb3654
Soeren Kroell <hallo@soerenkroell.com>
2015-07-04 12:12:14 +0200

    [TASK] Viele Anpassungen

--- Resources/Public/Js/Script.js
+++ Resources/Public/Js/Script.js
@@ -4,4 +4,62 @@ $(document).ready(function () {
 	});
 
 	$(".fancybox").fancybox();
+
+
+
+
+    /**
+     * =======================================
+     * Function: Detect Mobile Device
+     * =======================================
+     */
+    // source: http://www.abeautifulsite.net/detecting-mobile-devices-with-javascript/
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match( /Android/i );
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match( /BlackBerry/i );
+        },
+        iOS: function() {
+            return navigator.userAgent.match( /iPhone|iPad|iPod/i );
+        },
+        Opera: function() {
+            return navigator.userAgent.match( /Opera Mini/i );
+        },
+        Windows: function() {
+            return navigator.userAgent.match( /IEMobile/i );
+        },
+        any: function() {
+            return ( isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows() );
+        }
+    };
+
+    /**
+     * =======================================
+     * Animations
+     * =======================================
+     */
+    if ( $( 'body' ).hasClass( 'enable-animations' ) && ! isMobile.any() ) {
+        var $elements = $( '*[data-animation]' );
+
+        console.log($elements);
+
+        $elements.each( function( i, el ) {
+
+            var $el = $( el ),
+                animationClass = $el.data( 'animation' );
+
+            $el.addClass( animationClass );
+            $el.addClass( 'animated' );
+            $el.addClass( 'wait-animation' );
+
+            $el.one( 'inview', function() {
+
+                $el.removeClass( 'wait-animation' );
+                $el.addClass( 'done-animation' );
+            });
+        });
+    };
+
 });
