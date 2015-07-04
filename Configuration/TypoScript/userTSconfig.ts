a3cd8895089c969e6d82707ffe46325c68038058
Soeren Kroell <hallo@soerenkroell.com>
2015-07-05 01:17:17 +0200

    [TASK] Allgemeine Settings erweitert

--- Configuration/TypoScript/userTSconfig.ts
+++ Configuration/TypoScript/userTSconfig.ts
@@ -11,24 +11,3 @@ options {
 	pageTree.doktypesToShowInNewPageDragArea = 1,4,3,254,199
 }
 
-
-
-
-ajaxCall = PAGE
-ajaxCall {
-    typeNum = 200
-    10 = USER
-    10 {
-        userFunc = TYPO3\CMS\Extbase\Core\Bootstrap->run
-        pluginName = OtWebsite
-        extensionName = OtWebsite
-        vendorName = OliverThiele
-    }
-    config {
-        disableAllHeaderCode = 1
-        additionalHeaders = Content-type:application/json
-        xhtml_cleaning = 0
-        admPanel = 0
-        no_cache = 1
-    }
-}
